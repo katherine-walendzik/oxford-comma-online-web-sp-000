7a9753570468a657acd925cc8a589425224095e8
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 17:06:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,10 @@
 def oxford_comma(array)
-  #last = array.pop
   
-  #if array.length > 2
-   # array.join(", ") + ", and " + last.to_s
+  if array.length > 2
+    array.join(", ") + ", and " + array.pop.to_s
   #elsif array.length == 2
    # array.join(" and ")
-  #else
+  else
     array.join
-  #end
+  end
 end
