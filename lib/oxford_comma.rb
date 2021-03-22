971e8d5c453d9673f435cfda1efb5e3aa5c918fc
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 17:20:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,7 @@
 def oxford_comma(array)
-    
-    if array.length == 2
-        array.join(" and ")
-    elsif array.length > 2
-        array.insert(-2, "and")
-        array[0..-1].join(", ")
-    else
-        array.join
-    end
+  return array.join(' and ') if array.size < 3
+    array[-1] = "and " + array[-1]
+    array.join(', ')
 end
 
 
