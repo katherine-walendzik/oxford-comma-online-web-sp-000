d8ae7e6a0e01e407ee2b4def9b56963c18b57d82
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 16:59:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
   last = array.pop
   
-  if array.length > 2
+  if array.length >= 2
     array.join(", ") + ", and " + last.to_s
-  elsif array.length = 2
-    array.join(" and ")
   else
     array.join
   end
